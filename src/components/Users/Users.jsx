56fa466d18d84251fe758623cbc21420d5a83720
Linkin <kovalchukruslanlink@gmail.com>
2022-09-15 21:55:23 +0300

    3

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,17 +1,27 @@
 import React from "react";
 import styles from './users.module.css';
+import axios from "axios";
 
 let Users = (props) => {
-    if (props.users.length === 0) {
-        props.setUsers( [
-            {id: 1, photoUrl: 'https://pbs.twimg.com/profile_images/1215070700026855425/7edvU72D_400x400.jpg', followed: true, fullName: "Ruslan", status: 'I am a boss', location: {city: 'Lviv', country: 'Ukraine'}},
-            {id: 2, photoUrl: 'https://pbs.twimg.com/profile_images/1215070700026855425/7edvU72D_400x400.jpg', followed: false, fullName: "Nastia", status: 'I am a boss', location: {city: 'Hmel', country: 'Ukraine'}},
-            {id: 3, photoUrl: 'https://pbs.twimg.com/profile_images/1215070700026855425/7edvU72D_400x400.jpg', followed: true, fullName: "Alex", status: 'I am a boss', location: {city: 'Kyiev', country: 'Ukraine'}},
-            {id: 4, photoUrl: 'https://pbs.twimg.com/profile_images/1215070700026855425/7edvU72D_400x400.jpg', followed: false, fullName: "Stas", status: 'I am a boss', location: {city: 'Rivne', country: 'Ukraine'}},
-        ])
+    let getUsers = ()=> {
+        if (props.users.length === 0) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+
+                props.setUsers(response.data.items)
+            })
+
+
+            // props.setUsers( [
+            //     {id: 1, photoUrl: 'https://pbs.twimg.com/profile_images/1215070700026855425/7edvU72D_400x400.jpg', followed: true, fullName: "Ruslan", status: 'I am a boss', location: {city: 'Lviv', country: 'Ukraine'}},
+            //     {id: 2, photoUrl: 'https://pbs.twimg.com/profile_images/1215070700026855425/7edvU72D_400x400.jpg', followed: false, fullName: "Nastia", status: 'I am a boss', location: {city: 'Hmel', country: 'Ukraine'}},
+            //     {id: 3, photoUrl: 'https://pbs.twimg.com/profile_images/1215070700026855425/7edvU72D_400x400.jpg', followed: true, fullName: "Alex", status: 'I am a boss', location: {city: 'Kyiev', country: 'Ukraine'}},
+            //     {id: 4, photoUrl: 'https://pbs.twimg.com/profile_images/1215070700026855425/7edvU72D_400x400.jpg', followed: false, fullName: "Stas", status: 'I am a boss', location: {city: 'Rivne', country: 'Ukraine'}},
+            // ])
+        }
     }
 
     return <div>
+        <button onClick={getUsers}>Get Users</button>
         {
             props.users.map(u => <div key={u.id}>
                 <span>
@@ -32,8 +42,8 @@ let Users = (props) => {
                         <div>{u.status}</div>
                     </span>
                     <span>
-                        <div>{u.location.country}</div>
-                        <div>{u.location.city}</div>
+                        <div>{'u.location.country'}</div>
+                        <div>{'u.location.city'}</div>
                     </span>
                 </span>
             </div>)
